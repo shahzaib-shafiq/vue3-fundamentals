7576ee12a047d3d041ef6d0c11a2a002e19c3335
shahzaibss <73356944+shahzaib-shafiq@users.noreply.github.com>
2024-11-24 18:37:12 +0500

    v-bind

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,6 +2,18 @@
 defineProps<{
   msg: string
 }>()
+
+// const width = 600
+// const height = 400
+// const imgUrl =
+//   'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAORfccCDlOsEHq-qQfY2una31CYsnzeO8cA&s'
+
+const imageInfo = {
+  width: 600,
+  height: 400,
+  imgUrl:
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAORfccCDlOsEHq-qQfY2una31CYsnzeO8cA&s',
+}
 </script>
 
 <template>
@@ -12,6 +24,9 @@ defineProps<{
       <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
     </h3>
+
+    <!-- <img :src="imgUrl" alt="" :width="width" :height="height" /> -->
+    <img v-bind="imageInfo" />
   </div>
 </template>
 
