d8a5b04716c2826bde001c36ec0407b44941c736
shahzaibss <sshafiq@aslase.com>
2024-11-24 15:33:45 +0500

    v-model

---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Home</h1>
+    <input type="text" placeholder="Email" v-model="email" />
+    <br />
+    <br />
+
+    <input type="password" placeholder="Password" v-model="password" />
+    <br />
+    <br />
+
+    <button @click="getData()">get Values</button>
+  </div>
+  {{ password }}
+</template>
+
+<script>
+const getData = () => {
+  return { email: null, password: null }
+}
+</script>
